22240cf0cf3791734618e3da71ab8233ddc7b917
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-17 12:37:35 +0300

    Code refactor

--- src/csv/CsvFileParser.java
+++ src/csv/CsvFileParser.java
@@ -1,6 +1,6 @@
 package csv;
 
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -31,54 +31,36 @@ public class CsvFileParser
 		fieldNameParserMap.put(fieldName, parser);
 	}
 	
-	public <T> List<T> parse(Class<T> type, String path)
+	public <T> List<T> parse(Class<T> type, String path, Charset encoding)
 	{
 		List<T> list = new ArrayList<T>();
 		
+		final String CSV_SEPARATOR = ",";
+		final String EMPTY_STRING = "";
+		
 		try
 		{
-			List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+			List<String> lines = Files.readAllLines(Paths.get(path), encoding);
 			
-			String[] columns = lines.remove(0).split(",");
+			String[] columns = lines.remove(0).split(CSV_SEPARATOR);
 			HashMap<String, Integer> nameIndexPairs = new HashMap<>();
 			
 			for (int index = 0; index < columns.length; index++)
 			{
-				nameIndexPairs.put(columns[index].replaceAll("_", "").toLowerCase(), index);
+				nameIndexPairs.put(columns[index].replaceAll("_", EMPTY_STRING).toLowerCase(), index);
 			}
 			
 			Field[] fields = type.getDeclaredFields();
+			Constructor<T> constructor = type.getConstructor(new Class<?>[0]);
 			
 			for (String line : lines)
 			{
-				String[] lineArgs = line.split(",");
-				Constructor<T> constructor = type.getConstructor(new Class<?>[0]);
+				String[] lineArguments = line.split(CSV_SEPARATOR);
 				T object = constructor.newInstance(new Object[0]);
 				
 				for (Field field : fields)
 				{	
-					String fieldName = field.getName().toLowerCase();
-					CsvName csvName = field.getAnnotation(CsvName.class);
-					
-					if (csvName != null)
-					{
-						fieldName = csvName.value();
-					}
-					
-					int argumentIndex = nameIndexPairs.get(fieldName);
-					String lineValueAsText = lineArgs[argumentIndex];
-					
-					Class<?> fieldType = field.getType();
-					Function<String, Object> parser = typeParserMap.get(fieldType);
-					
-					if (fieldNameParserMap.containsKey(fieldName))
-					{
-						parser = fieldNameParserMap.get(fieldName);
-					}
-					
-					Object fieldValue = parser.apply(lineValueAsText);
-					
-					field.set(object, fieldValue);
+					setObjectField(object, field, lineArguments, nameIndexPairs);
 				}
 				
 				list.add(object);
@@ -90,6 +72,34 @@ public class CsvFileParser
 		}
 		
 		return list;
+	}
+	
+	private <T> void setObjectField(T object, Field field, String[] lineArguments,
+								    HashMap<String, Integer> nameIndexPairs) throws Exception
+	{
+		String fieldName = field.getName().toLowerCase();
+		CsvName csvName = field.getAnnotation(CsvName.class);
+		
+		Function<String, Object> customParser = fieldNameParserMap.get(fieldName);
+		
+		if (csvName != null)
+		{
+			fieldName = csvName.value();
+		}
+		
+		int argumentIndex = nameIndexPairs.get(fieldName);
+		String lineValueAsText = lineArguments[argumentIndex];
+		
+		Class<?> fieldType = field.getType();
+		Function<String, Object> parser = typeParserMap.get(fieldType);
+		
+		if (customParser != null)
+		{
+			parser = customParser;
+		}
+		
+		Object fieldValue = parser.apply(lineValueAsText);
+		field.set(object, fieldValue);
 	}
 	
 	private void initializeTypeParserMap()
--- src/tests/Main.java
+++ src/tests/Main.java
@@ -1,5 +1,6 @@
 package tests;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import csv.CsvFileParser;
@@ -9,8 +10,8 @@ public class Main
 	public static void main(String[] args)
 	{
 		CsvFileParser csv = new CsvFileParser();
-		csv.addFieldNameParser("status", x -> x.equals("old"));
-		List<Product> products = csv.parse(Product.class, "resources/products.csv");
+		csv.addFieldNameParser("old", x -> x.equals("old"));
+		List<Product> products = csv.parse(Product.class, "resources/products.csv", StandardCharsets.UTF_8);
 		products.stream().forEach(x -> System.out.println(x.name + " " + (x.price + 10) + " " + x.symbol + " " + x.old));
 	}
 }
